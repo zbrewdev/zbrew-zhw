fbf38842a93e692788dc3a544865175bbe0d25dc
Mike Fulton <fultonm@ca.ibm.com>
2020-01-15 16:10:04 -0600

    create a deploy.sh #4 mikefultonbluemix/zbrew#30

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,34 @@
 #!/bin/sh
-exit 0
+#set -x
+function syntax {
+	echo "Syntax: deploy.sh <directory>"
+	echo " deploy the bill of materials to the specified directory"
+	return 0
+}
+
+if [ $# -ne 1 ]; then
+	echo "Need to provide a directory to deploy to. No parameter given."
+	syntax
+	exit 16
+fi
+if [ ! -d $1 ]; then 
+	echo "Need to specify a directory to deploy to. $1 is not a directory."
+	syntax
+	exit 16
+fi
+
+rm -rf $1/zhw110
+mkdir $1/zhw110
+mkdir $1/zhw110/docs
+mkdir $1/zhw110/docs/C
+mkdir $1/zhw110/docs/C/cat1
+mkdir $1/zhw110/smphold
+mkdir $1/zhw110/smpmcs
+mkdir $1/zhw110/smprelf
+
+names=`cat zhw110.bom`
+code="${names}"
+
+cp -p ${code} $1
+
+exit $? 

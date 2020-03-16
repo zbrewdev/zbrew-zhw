26940c192ee71c77b95607b599c78f72fe63a9db
OMVSKERN <mikefultongithub@gmail.com>
2020-03-16 15:59:01 -0400

    change script a bit to not use /zbrewxx but instead /zbrew/xx

--- tests/zhwoverride.sh
+++ tests/zhwoverride.sh
@@ -2,33 +2,10 @@
 #
 # Install zhw with a few overrides and then ensure the zFS files are laid down
 #
-. zbrewfuncs
-mydir=$(callerdir ${0})
-props="${mydir}/../../zbrew/zbrewglobalprops.json"
-zbrewpropse zbrew config "${props}"
+. zbrewsetenv
 
-#set -x
+set -x
 sw='zhw110'
-zosname=$(echo ${sw} | tr '[:lower:]' '[:upper:]');
-ussname=$(echo ${sw} | tr '[:upper:]' '[:lower:]');
-prefix=`echo "${ussname}" | awk '{ print substr($1, 0, 3) }'`
-
-smpelibs="${ZBREW_WORKROOT}/zbrew-${prefix}/${ussname}/${ussname}bom.json"
-
-libs=`readbom ${zosname} bomfiles <${smpelibs}`
-# Obtain list of ZFS and allocate/mount
-ds=`echo "${libs}" | awk -v pfx="${ZBREW_SRC_HLQ}${zosname}." '($2 == "ZFS") {print pfx""$1","}' | tr -d "\n"`
-zfscnt=`echo "${ds}" | awk -F, '{}END {print NF}'`
-while [ $zfscnt -ge 1 ]; 
-do
-        zfsds=`echo "${ds}" | awk -F, -v zfsv=$zfscnt '{print $zfsv}'`
-	if [ "$zfsds" != "" ]; then 
-        	unmount -f ${zfsds} 2>/dev/null
-	fi
-	zfscnt=`expr $zfscnt - 1`
-done
-
-drm -f "${ZBREW_SRC_HLQ}zhw*.*"
 
 zosinfo=`uname -rsvI`
 version=`echo ${zosinfo} | awk '{ print $3; }'`
@@ -43,20 +20,24 @@ case ${release} in
 		;;
 esac
 
+dls 'zbrewvs.*'
+
 zbrew install zhw110
 rc=$?
 if [ $rc != 0 ]; then
 	echo "zbrew install failed with rc:$rc" >&2
-	exit 3
+	exit 2
 fi
 
+dls 'zbrewvs.*'
 
 zbrew -c install zhw110
 rc=$?
 if [ $rc != 0 ]; then
         echo "zbrew install failed with rc:$rc" >&2
-        exit 5
+        exit 3
 fi
+dls 'zbrewvt.*'
 
 zbrew smpreceiveptf zhw110 MCSPTF2  
 rc=$?
@@ -64,29 +45,36 @@ if [ $rc != 0 ]; then
         echo "zbrew receive ptf from z/os file failed with rc:$rc" >&2
         exit 4
 fi
+dls 'zbrewvt.*'
 
 zbrew update zhw110
 rc=$?
 if [ $rc != 0 ]; then
         echo "zbrew update of zhw110 failed with rc:$rc" >&2
-        exit 4
+        exit 5
 fi
-
+dls 'zbrewvt.*'
 
 zbrew configure zhw110
 rc=$?
 if [ $rc != 0 ]; then
         echo "zbrew configure failed with rc:$rc" >&2
-        exit 4
+        exit 6
 fi
+dls 'zbrewvt.*'
 
+zbrew deconfigure zhw110
+rc=$?
+if [ $rc != 0 ]; then
+        echo "zbrew uninstall failed with rc:$rc" >&2
+        exit 7
+fi
 
 zbrew uninstall zhw110
 rc=$?
 if [ $rc != 0 ]; then
         echo "zbrew uninstall failed with rc:$rc" >&2
-        exit 6
+        exit 8
 fi
-
 
 exit 0
